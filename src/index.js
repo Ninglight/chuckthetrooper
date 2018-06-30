e363b1938f4dbbce93b28f0540424e1163cbf6d8
Ninglight <valentin.boureau@gmail.com>
2018-06-30 18:55:21 +0200

    init webpack configuration for integration way

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './scss/app.scss';
+
+import logo from './icons/logo.svg';
+
+
+function component() {
+
+    return console.log('ça marche bien !');
+}
+
+component();
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        js: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+};
