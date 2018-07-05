556d5386bd38a34b032bae7f067c90152bc0cfa4
Ninglight <valentin.boureau@gmail.com>
2018-07-05 09:47:07 +0200

    add clear webpack process with dev and production mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-
 // import from React
 import React from 'react' // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom' // eslint-disable-line no-unused-vars
-import { HashRouter } from 'react-router-dom' // eslint-disable-line no-unused-vars
+import {HashRouter} from 'react-router-dom' // eslint-disable-line no-unused-vars
 import registerServiceWorker from './registerServiceWorker' // eslint-disable-line no-unused-vars
 import App from './routes' // eslint-disable-line no-unused-vars
 
 // import styles
 import './scss/app.scss'
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!')
+}
 
 ReactDOM.render((
   <HashRouter>
--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,75 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      // Task to js file : linter
+      {
+        test: /\.(js|jsx|mjs)$/,
+        enforce: 'pre',
+        use: ['eslint-loader']
+      },
+      // Task to js file
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      },
+      // Task to sass file
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              minimize: true
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      // Task to images files
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      // Task to fonts files
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
+
+
+/* const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
@@ -46,7 +117,7 @@ module.exports = {
           },
           {
             loader: "css-loader",
-            options : {
+            options: {
               minimize: true
             }
           },
@@ -79,4 +150,4 @@ module.exports = {
     })
   ],
   watch: true
-};
+}; */
